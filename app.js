342ef912da3adce4b0d5a7e9df9c249b404ef219
Daniello109 <legolas_lorien@hotmail.com>
2022-11-06 19:00:03 +0100

    quete.sample

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
-const MY_NAME = process.env.MY_NAME;
-const MY_CITY = process.env.MY_CITY;
-const MY_LANGUAGE = process.env.MY_LANGUAGE;
+const MY_NAME = process.env.sample.MY_NAME;
+const MY_CITY = process.env.sample.MY_CITY;
+const MY_LANGUAGE = process.env.sample.MY_LANGUAGE;
 
 console.log(`I am ${MY_NAME}, wilder in ${MY_CITY}, and I love ${MY_LANGUAGE}`);
 
